393819f412b989a3331bba084e3fce435e7bd446
moshunger <moshunger@81400000-312d-67bb-07e7-a3f3d5d0ccb6>
2011-06-01 08:51:54 +0000

    not relevant

--- wsep112-web/src/web/client/WebControllerAsync.java
+++
@@ -1,9 +0,0 @@
-package web.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface WebControllerAsync {
-
-	void login(String username, String password, AsyncCallback<Boolean> callback);
-
-}
